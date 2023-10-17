cc4f57cd083758f4dae122e3b26615b0263a0f48
Core <daverencordero@gmail.com>
2023-10-17 10:37:38 +0800

    created a 3D line
    - learned about model view perspective matrices
    - learned about perspective projection

--- examples/common/transform.rs
+++ examples/common/transform.rs
@@ -17,3 +17,107 @@ pub fn create_transforms(
     // return final model matrix
     model_mat
 }
+
+use cgmath::*;
+use std::f32::consts::PI;
+use winit::window::Window;
+
+pub struct InitWgpu {
+    pub surface: wgpu::Surface,
+    pub device: wgpu::Device,
+    pub queue: wgpu::Queue,
+    pub config: wgpu::SurfaceConfiguration,
+    pub size: winit::dpi::PhysicalSize<u32>,
+}
+
+impl InitWgpu {
+    pub async fn init_wgpu(window: &Window) -> Self {
+        let size = window.inner_size();
+        let instance = wgpu::Instance::new(wgpu::Backends::all());
+        let surface = unsafe { instance.create_surface(window) };
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .unwrap();
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                },
+                None, // Trace path
+            )
+            .await
+            .unwrap();
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface.get_preferred_format(&adapter).unwrap(),
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+        surface.configure(&device, &config);
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+        }
+    }
+}
+
+#[rustfmt::skip]
+#[allow(unused)]
+
+pub const OPENGL_TO_WGPU_MATRIX: Matrix4<f32> = Matrix4::new(
+1.0, 0.0, 0.0, 0.0,
+0.0, 1.0, 0.0, 0.0,
+0.0, 0.0, 0.5, 0.0,
+0.0, 0.0, 0.5, 1.0,
+);
+
+pub fn create_view_projection(
+    camera_position: Point3<f32>,
+    look_direction: Point3<f32>,
+    up_direction: Vector3<f32>,
+    aspect: f32,
+    is_perspective: bool,
+) -> (Matrix4<f32>, Matrix4<f32>, Matrix4<f32>) {
+    // construct view matrix
+    let view_mat = Matrix4::look_at_rh(camera_position, look_direction, up_direction);
+    // construct projection matrix
+    let project_mat: Matrix4<f32>;
+    if is_perspective {
+        project_mat = OPENGL_TO_WGPU_MATRIX * perspective(Rad(2.0 * PI / 5.0), aspect, 0.1, 100.0);
+    } else {
+        project_mat = OPENGL_TO_WGPU_MATRIX * ortho(-4.0, 4.0, -3.0, 3.0, -1.0, 6.0);
+    }
+    // construct view-projection matrix
+    let view_project_mat = project_mat * view_mat;
+    // return various matrices
+    (view_mat, project_mat, view_project_mat)
+}
+
+pub fn create_view(
+    camera_position: Point3<f32>,
+    look_direction: Point3<f32>,
+    up_direction: Vector3<f32>,
+) -> Matrix4<f32> {
+    Matrix4::look_at_rh(camera_position, look_direction, up_direction)
+}
+
+pub fn create_projection(aspect: f32, is_perspective: bool) -> Matrix4<f32> {
+    let project_mat: Matrix4<f32>;
+    if is_perspective {
+        project_mat = OPENGL_TO_WGPU_MATRIX * perspective(Rad(2.0 * PI / 5.0), aspect, 0.1, 100.0);
+    } else {
+        project_mat = OPENGL_TO_WGPU_MATRIX * ortho(-4.0, 4.0, -3.0, 3.0, -1.0, 6.0);
+    }
+    project_mat
+}
